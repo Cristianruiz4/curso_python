3ab5a25d823a13d7cc432b5341a33e96aa779d7b
Cristianruiz4 <dareyruiz4@gmail.com>
2025-01-27 10:34:03 -0700

    Modifiqué el programa para funcionar con floats

--- linea/funciones.py
+++ linea/funciones.py
@@ -1,23 +1,22 @@
 # archivo con todas las funciones necesarias para la aplicacion "linea"
 
-def calcular_y(x, m, b):
+def calcular_y(x:float, m:float, b:float)->float:
     '''
     b: interseccion en y
     regresa el valor de y
     '''
 
-    return (m*x) + b
+    return m*x + b
 
 def test_linea():
 
     '''
     Prueba de funcionamiento de calcular_y
-    '''
+    ''' 
+    return calcular_y(0.0, 2.0, 3.0)
 
-    return calcular_y(0,2,3)
-
-    if __name__ == '__main__':
-        if test_linea() == 3:
-            print('Test exitoso')
-        else:
-            print('Test fallido')
+if __name__ == '__main__':
+    if test_linea() == 3.0:
+        print('Test exitoso')
+    else:
+        print('Test fallido')
