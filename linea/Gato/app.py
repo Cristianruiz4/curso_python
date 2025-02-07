dad1d89910b365fceaf38d0abc0792e281f6d8bb
Cristianruiz4 <dareyruiz4@gmail.com>
2025-02-07 10:56:35 -0700

    Agregue funciones para mostrar los puntajes de cada jugador

---
+++ linea/Gato/app.py
@@ -0,0 +1,24 @@
+'''
+Este archivo es el punto de entrada a la aplicación. Aquí se importan las funciones de tablero.py y se ejecutan en un ciclo while.
+'''
+import Tablero
+
+def main():
+    ''' Función principal '''
+    X = {"G":0,"P":0,"E":0}
+    O = {"G":0,"P":0,"E":0}
+    score = {"X":X,"O":O}
+    numeros =[str(i) for i in range(1,10)]
+    corriendo = True
+    while corriendo:
+        dsimbolos = {x:x for x in numeros}
+        g = Tablero.juego(dsimbolos)
+        Tablero.actualiza_score(score,g)
+        Tablero.despliega_tablero(score)
+        seguir = input('¿Quieres seguir? (s/n): ')
+        if seguir.lower() == 'n':
+            corriendo = False
+
+
+if __name__ == '__main__':
+    main()
