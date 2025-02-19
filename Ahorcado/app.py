ffe6359882a854118c01e9cbf1a47d62d8eb62df
Cristianruiz4 <dareyruiz4@gmail.com>
2025-02-18 20:21:22 -0700

    Terminamos el juego del ahorcado

---
+++ Ahorcado/app.py
@@ -0,0 +1,41 @@
+'''
+Programa principal del juego ahorcado
+'''
+
+import os
+import string
+import unicodedata
+import argparse
+from random import choice
+import funciones as fn
+
+def main(archivo_texto:str, nombre_plantilla='plantilla'):
+    '''
+    Programa principal
+    '''
+    #cargamos las plantillas
+    plantillas = fn.carga_archivo_texto(nombre_plantilla)
+    lista_oraciones = fn.carga_archivo_texto(archivo_texto)
+    palabras = fn.obten_palabras(lista_oraciones)
+    o = 5 #oportunidades
+    p = choice(palabras)
+    abcdario = {letra:letra for letra in string.ascii_lowercase}
+    adivinadas = set()
+    while o > 0:
+        fn.despliega_plantilla(plantillas, o)
+        o = fn.adivina_letra(abcdario, p, adivinadas, o)
+        if p == ''.join([letra if letra in adivinadas else '_' for letra in p]):
+            print('Ganaste')
+            break
+        fn.despliega_plantilla(plantillas, o)
+        print(f"La palabra era: {p}")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(decription='Juego del ahorcado')
+    parser.add_argument('-a','--archivo', help='Archivo de texto con palabras a adivinar', default='./datos/pg15532.txt')
+    args = parser.parse_args()
+    archivo = args.archivo
+    if os.stat(archivo) is False:
+        print(f'El archivo {archivo} no existe')
+    else:
+        main(archivo)
