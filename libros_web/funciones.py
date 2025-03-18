99b094fb77df58a794fb8211ae74cef99066e0bf
Cristianruiz4 <dareyruiz4@gmail.com>
2025-03-18 10:11:19 -0700

    Terminamos el programa Libros_web

--- libros_web/funciones.py
+++ libros_web/funciones.py
@@ -31,6 +31,10 @@ def busca_en_diccionario(diccionario:dict, palabra:str)->list:
             lista.append(libro)
     return lista
 
+def libros_empiezan_con(lista:list, letra:str)->list:
+    ''' Busca libros que empiecen con letra '''
+    return [x for x in lista if x['title'].lower().startswith(letra.lower())]
+
 if __name__ == '__main__':
     archivo_csv = 'booklist2000.csv'
     lista_libros = lee_archivo_csv(archivo_csv)
